40a751abd5316639e78d6e654257e2dcd54d8698
ZeGuilhermeYung <jgryung@gmail.com>
2023-10-30 18:42:37 -0300

    correcting date comparison

--- src/services/flights.services.js
+++ src/services/flights.services.js
@@ -1,12 +1,11 @@
-import dayjs from "dayjs";
 import { conflictError, notFound, unprocessableEntity } from "../errors/errors.js";
 import { citiesRepositories } from "../repositories/cities.repositories.js";
 
 async function flightIsPossible(origin, destination, date) {
   const originCity = await citiesRepositories.findCityById(origin);
   const destinationCity = await citiesRepositories.findCityById(destination);
-  const today = dayjs().format("DD-MM-YYYY");
-  console.log(date, today)
+  const splitData = date.split("-");
+  const flightDate = new Date(splitData[2], splitData[1] - 1, splitData[0]);
 
   if (!originCity)
     throw notFound("A cidade de origem não existe no banco de dados!");
@@ -14,7 +13,7 @@ async function flightIsPossible(origin, destination, date) {
     throw notFound("A cidade de destino não existe no banco de dados!");
   if (origin === destination)
     throw conflictError("Origem e destino não podem ser a mesma cidade!");
-  if (today.isAfter(date))
+  if(flightDate < new Date())
     throw unprocessableEntity("a data não pode ser anterior que a data atual!");
 }
 
